cc05eb221e38505b39b82d255f3c6f0d828ffdc7
Lockefox <locke.renard@gmail.com>
2016-05-17 09:41:58 -0700

    added logging module (untested)
    rearranged imports to conform with pylint

--- crest_endpoint.py
+++ crest_endpoint.py
@@ -1,10 +1,14 @@
-from flask import Flask, Response, jsonify, abort, Markup
-from flask_restful import reqparse, Api, Resource, request
-from flaskext.markdown import Markdown
+'''crest_endpoint.py: CREST centric REST API endpoints'''
+
 import datetime
 import os
 import json
 import configparser
+import logging
+from logging.handlers import TimedRotatingFileHandler, SMTPHandler
+from flask import Flask, Response, jsonify, abort, Markup
+from flask_restful import reqparse, Api, Resource, request
+from flaskext.markdown import Markdown
 import requests
 
 #### CONFIG PARSER ####
@@ -21,12 +25,55 @@ app = Flask(__name__)
 api = Api(app)
 md  = Markdown(app)
 
+#### LOGGING STUFF ####
+LOG_ABSPATH = os.getcwd() + config.get('LOGGING', 'log_path')
+LOG_NAME    = __file__.replace('.py', '.log')
+LOG_FORMAT  = '%(asctime)s;%(levelname)s;%(funcName)s;%(message)s'
+LOG_LEVEL   = config.get('LOGGING', 'log_level')
+LOG_FREQ    = config.get('LOGGING', 'log_freq')
+LOG_TOTAL   = config.get('LOGGING', 'log_total')
+EMAIL_TITLE = __file__.replace('.py', '') + " CRITICAL ERROR"
+EMAIL_TOLIST= str(config.get('LOGGING', 'email_recipients')).split(',')
+Logger = logging.getLogger(__file__)
+
+#### LOGGING UTILITIES ####
+def log_setup():
+    '''Sets up logging object for app'''
+    Logger.setLevel(LOG_LEVEL)
+    generalHandler = TimedRotatingFileHandler(
+        LOG_ABSPATH,
+        when        = LOG_LEVEL,
+        interval    = 1,
+        backupCount = LOG_TOTAL
+    )
+    formatter = logging.Formatter(LOG_FORMAT)
+    generalHandler.setFormatter(formatter)
+    Logger.addHandler(generalHandler)
+
+    #TODO: replace email handler with log scraper?
+    emailHandler = SMTPHandler(
+        mailhost    = config.get('LOGGING', 'email_server') + ':' + \
+                      config.get('LOGGING', 'email_port'),
+        fromaddr    = config.get('LOGGING', 'email_fromaddr'),
+        toaddrs     = EMAIL_TOLIST,
+        subject     = EMAIL_TITLE,
+        credentials = (config.get('LOGGING', 'email_username'),
+                       config.get('LOGGING', 'email_secret'))
+        )
+    emailHandler.setLevel(config.get('LOGGING', 'log_email_level'))
+    emailHandler.setFormatter(formatter)
+    Logger.addHandler(emailHandler)
+
+def email_body_builder(errorMsg, helpMsg):
+    '''Builds email message for easier reading with SMTPHandler'''
+    None
 
 if __name__ == '__main__':
+    log_setup()
     if BOOL_DEBUG_ENABLED:
         app.run(debug=True)
     else:
         app.run(
-            host='0.0.0.0',
-            port=CREST_FLASK_PORT
+            host = '0.0.0.0',
+            port = CREST_FLASK_PORT
         )
