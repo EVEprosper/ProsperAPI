4a5aece03aad6b644d0273d993360eeceb426995
Lockefox <locke.renard@gmail.com>
2016-05-22 09:32:01 -0700

    moving config from .ini to .cfg

--- common/prosperAPI_utility.py
+++ common/prosperAPI_utility.py
@@ -4,12 +4,17 @@ import datetime
 import os
 import json
 import configparser
+from configparser import ExtendedInterpolation
 import requests
 
-CONFIG_FILE = 'init.ini' #TODO: change to .cfg?
+CONFIG_FILE = 'prosperAPI.cfg' #TODO: change to .cfg?
 
 def get_config(dir=''):
-    config = configparser.ConfigParser()
+    config = configparser.ConfigParser(
+        interpolation  = ExtendedInterpolation(),
+        allow_no_value = True,
+        delimiters     = ('=')
+    )
     config_filepath = os.path.join(dir, CONFIG_FILE)
     
     if os.path.exists(config_filepath):
@@ -17,3 +22,6 @@ def get_config(dir=''):
             config.read_file(filehandle)
         return config
     return None
+    
+def create_logger(log_name, log_level='INFO'):
+    
