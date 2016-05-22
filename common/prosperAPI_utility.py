3aed99b836f1ca0a4e250321f34d73d6cd0024ab
Lockefox <locke.renard@gmail.com>
2016-05-22 10:05:22 -0700

    updated create_logger general utility

--- common/prosperAPI_utility.py
+++ common/prosperAPI_utility.py
@@ -16,12 +16,50 @@ def get_config(dir=''):
         delimiters     = ('=')
     )
     config_filepath = os.path.join(dir, CONFIG_FILE)
-    
+
     if os.path.exists(config_filepath):
         with open(config_filepath, 'r') as filehandle:
             config.read_file(filehandle)
         return config
     return None
-    
-def create_logger(log_name, log_level='INFO'):
-    
+
+def create_logger(logName, logLevel_override = ''):
+    tmpConfig = get_config()
+
+    logFolder = os.path.join('..', tmpConfig['LOGGING', 'logFolder'])
+    if not os.path.exists(logFolder):
+        os.makedirs(logFolder)
+
+    logLevel = tmpConfig['LOGGING', 'logLevel']
+    logFreq  = tmpConfig['LOGGING', 'logFreq']
+    logTotal = tmpConfig['LOGGING', 'logTotal']
+    logName  = logName + '.log'
+
+    if logLevel_override:
+        logLevel = logLevel_override
+
+    logFullpath = os.path.join(logFolder, logName)
+    generalHandler = TimedRotatingFileHandler(
+        logFullpath,
+        when        = logFreq,
+        interval    = 1,
+        backupCount = logTotal
+    )
+
+    emailSource     = tmpConfig['LOGGING', 'emailSource']
+    emailRecipients = tmpConfig['LOGGING', 'emailRecipients']
+    emailUsername   = tmpConfig['LOGGING', 'emailUsername']
+    emailFromaddr   = tmpConfig['LOGGING', 'emailFromaddr']
+    emailSecret     = tmpConfig['LOGGING', 'emailSecret']
+    emailServer     = tmpConfig['LOGGING', 'emailServer']
+    emailPort       = tmpConfig['LOGGING', 'emailPort']
+
+    bool_doEmail = (
+        emailSource     and
+        emailRecipients and
+        emailUsername   and
+        emailFromaddr   and
+        emailSecret     and
+        emailServer     and
+        emailPort
+    )
