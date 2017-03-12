2c4e60d0457dc34eb08d79eaa9c693ca2a233bbd
lockefox <locke.renard@gmail.com>
2017-03-12 16:33:27 -0700

    adding basic settings to manager.py

---
+++ scripts/manager.py
@@ -0,0 +1,34 @@
+"""manager.py: Flask-Script launcher for services
+
+using https://github.com/yabb85/ueki as prototype
+"""
+
+from flask_script import Manager, Server
+
+from prosper.publicAPI import create_app
+
+SETTINGS = {
+    'port':8001
+}
+
+app = create_app(SETTINGS)
+
+manager = Manager(app)
+manager.add_command(
+    'runserver',
+    Server(
+        host='0.0.0.0',
+        port=SETTINGS['port']
+    )
+)
+manager.add_command(
+    'debug',
+    Server(
+        host='localhost',
+        debug=True,
+        port=SETTINGS['port']
+    )
+)
+
+if __name__ == '__main__':
+    manager.run()
