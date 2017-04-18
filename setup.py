42985cf6788519d0d808a51c2c23a95b3b338112
Lockefox <locke.renard@gmail.com>
2017-04-18 12:31:28 -0700

    removing python-coveralls from setup.py

--- setup.py
+++ setup.py
@@ -123,8 +123,7 @@ setup(
         'pytest~=3.0.0',
         'pytest_cov~=2.4.0',        #requires requests==2.13.0
         'pytest-flask~=0.10.0',
-        'pymysql~=0.7.10'#,
-        #'python-coveralls~=2.9.1'
+        'pymysql~=0.7.10'
     ],
     cmdclass={
         'test':PyTest
