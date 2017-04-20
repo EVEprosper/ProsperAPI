ae84d542608927c0ed302cc675991bd26e9bbd83
Lockefox <locke.renard@gmail.com>
2017-04-20 10:55:23 -0700

    explicitly declairing tinymongo==0.1.8.dev0

--- setup.py
+++ setup.py
@@ -115,7 +115,7 @@ setup(
         'pystan~=2.14.0',
         'fbprophet~=0.1.post1',     #order matters: need pystan/cython first
         'tinydb~=3.2.2',
-        'tinymongo~=0.1.8',
+        'tinymongo~=0.1.8.dev0',
         'ujson~=1.35',
         'plumbum~=1.6.3',
         'shortuuid~=0.5.0'
